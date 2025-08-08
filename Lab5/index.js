12b1462b6d077b3f6d695082f76b6bd1845cbed0
jpanct <jlongct@gmail.com>
2025-08-08 19:16:44 -0400

    Merge remote with local

--- Lab5/index.js
+++ Lab5/index.js
@@ -2,7 +2,7 @@ import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
 import WorkingWithArrays from "./WorkingWithArrays.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
-import Hello from "./Hello.js";
+import Hello from "../Hello.js";
 export default function Lab5(app) {
   app.get("/lab5/welcome", (req, res) => {
     res.send("Welcome to Lab 5");
